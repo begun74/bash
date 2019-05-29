ab833928e0e51e3ce6447ebef7a873cdc26c4ce8
begun74 <begun74@gmail.com>
2019-05-29 17:26:16 +0300

    v 0.1

--- unitess_temp.sh
+++ unitess_temp.sh
@@ -7,11 +7,16 @@ SENSOR_ID=$1
 FILE=$2
 
 
-while read LINE; do
-#№     echo "Это строка: $LINE"
-#	sed -n $1 $LINE
+if [ -z $SENSOR_ID ] || [ -z $FILE ]
+then
+	echo "usage: ./unitess_temp.sh <SENSOR_ID> <FILE>"
+	exit 
+fi
 
-	echo 'LINE - ' $LINE
+
+while read LINE; do
+
+	#echo 'LINE - ' $LINE
 	declare -a MY_ARRAY=($LINE)
 	#echo 'MY_ARRAY - ' ${MY_ARRAY[0]}' '
 
